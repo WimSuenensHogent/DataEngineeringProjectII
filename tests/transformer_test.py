0d12ad89e5f7a0ca1029bb28c9e5594d981845c5
Ilya Mikhaylov <ilya.mikhaylov@student.hogent.be>
2021-11-18 19:20:32 +0100

    first test on transformer

--- tests/transformer_test.py
+++ tests/transformer_test.py
@@ -14,10 +14,9 @@ from tests.dummy_data_loader import DummyDataLoader
 
 class TestTransformer(unittest.TestCase):
 
-    def test_print(self):
+    def test_drop_columns_returns_data_frame_with_given_dropped_columns(self):
         dummy = DummyDataLoader()
         df = dummy.load("vaccinaties.csv")
-        print(len(df.columns))
         with open(
                 os.path.join(BASE_DIR, "tests/dummydata/dummy.json"), mode="r", encoding="UTF-8"
         ) as file:
@@ -25,8 +24,20 @@ class TestTransformer(unittest.TestCase):
             pipeline = data['pipeline'][0]
             columns_to_delete = pipeline["tranforms"][0]["data"]
             upd_df = Transformer.drop_columns(self, df, columns_to_delete)
-            print(upd_df)
             self.assertEqual(17, len(upd_df.columns))
+
+    def test_rename_columns_returns_data_frame_with_renamed_columns(self):
+        dummy = DummyDataLoader()
+        df = dummy.load("vaccinaties.csv")
+        with open(
+                os.path.join(BASE_DIR, "tests/dummydata/dummy.json"), mode="r", encoding="UTF-8"
+        ) as file:
+            data = json.load(file)
+            pipeline = data['pipeline'][0]
+            columns_to_delete = pipeline["tranforms"][1]["data"]
+            upd_df = Transformer.rename_columns(self, df, columns_to_delete)
+            self.assertEqual("nis_code", upd_df.columns[0])
+
 
 if __name__ == '__main__':
     unittest.main()
